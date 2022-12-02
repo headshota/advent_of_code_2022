f17c4aa01110eb330c88bde014f42df0d1f5d6e5
Shota Bakuradze <shota.bakuradze@experteer.com>
2022-12-02 21:41:33 +0100

    Complete day 1 both tasks, some minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod tasks;
 
 fn main() {
-    println!("Day 1 result is: {}", tasks::day_1());
+    println!("Day 1, part 1 result is: {}", tasks::day_1_1());
+    println!("Day 1, part 2 result is: {}", tasks::day_1_2());
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,11 +1,13 @@
 use std::fs;
 use std::io::{prelude::*, BufReader};
 
-pub fn day_1() -> u32{
-    let file = fs::File::open("src/inputs/day_1.txt")
-        .expect("Failed to read input file");
+fn get_input_lines(path: &str) -> Result<std::io::Lines<BufReader<fs::File>>, std::io::Error> {
+    let file = fs::File::open(path)?;
+    Ok(BufReader::new(file).lines())
+}
 
-    let lines = BufReader::new(file).lines();
+pub fn day_1_1() -> u32{
+    let lines = get_input_lines("src/inputs/day_1.txt").unwrap();
 
     let mut sum = 0;
     let mut max = 0;
@@ -22,7 +24,29 @@ pub fn day_1() -> u32{
             sum += line.parse::<u32>().unwrap();
         }
     }
-    
 
     max
 }
+
+
+pub fn day_1_2() -> u32 {
+    let lines = get_input_lines("src/inputs/day_1.txt").unwrap();
+
+    let mut elves = Vec::new();
+    let mut sum = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        if line.is_empty() {
+            elves.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<u32>().unwrap();
+        }
+    }
+    
+    elves.sort();
+    elves.reverse();
+    elves.iter().take(3).sum()
+}
+
