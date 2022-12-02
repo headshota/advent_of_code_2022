cf613b38b77ab43df6b3ac10d452dab60d5c8909
Shota Bakuradze <shota.bakuradze@experteer.com>
2022-12-02 21:46:36 +0100

    Organize day solutions into separate files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod tasks;
 
 fn main() {
-    println!("Day 1, part 1 result is: {}", tasks::day_1_1());
-    println!("Day 1, part 2 result is: {}", tasks::day_1_2());
+    println!("Day 1, part 1 result is: {}", tasks::day_1::part_1());
+    println!("Day 1, part 2 result is: {}", tasks::day_1::part_2());
 }
---
+++ src/tasks/day_1.rs
@@ -0,0 +1,51 @@
+use std::fs;
+use std::io::{prelude::*, BufReader};
+
+fn get_input_lines(path: &str) -> Result<std::io::Lines<BufReader<fs::File>>, std::io::Error> {
+    let file = fs::File::open(path)?;
+    Ok(BufReader::new(file).lines())
+}
+
+pub fn part_1() -> u32{
+    let lines = get_input_lines("src/inputs/day_1.txt").unwrap();
+
+    let mut sum = 0;
+    let mut max = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        if line.is_empty() {
+            if sum > max {
+                max = sum;
+            }
+
+            sum = 0;
+        } else {
+            sum += line.parse::<u32>().unwrap();
+        }
+    }
+
+    max
+}
+
+
+pub fn part_2() -> u32 {
+    let lines = get_input_lines("src/inputs/day_1.txt").unwrap();
+
+    let mut elves = Vec::new();
+    let mut sum = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        if line.is_empty() {
+            elves.push(sum);
+            sum = 0;
+        } else {
+            sum += line.parse::<u32>().unwrap();
+        }
+    }
+    
+    elves.sort();
+    elves.reverse();
+    elves.iter().take(3).sum()
+}
